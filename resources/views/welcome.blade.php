a340f76694d81eebe96e6f52498ff5317289ccaf
Elhadji <damediagnea@gmail.com>
2020-08-18 12:48:07 +0200

    Creation de ma base de donner via sqlite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,15 @@
 
 The best url shortener our there
 
-<form method="post" action="#">
+<form method="post" action="/">
+@csrf
 
 <div class="form-group">
   <div class="input-group">
     <span class="input-group-addon">URL</span>
-    <input type="text" class="form-control" placeholder="Enter your original URL here">
+    <input type="text" class="form-control" name="url"  placeholder="Enter your original URL here">
     <span class="input-group-btn">
-      <button class="btn btn-default" type="button">Shortener Url</button>
+      <input class="btn btn-default" type="submit" value="Shortener url">
     </span>
   </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/', function () {
+    dd(request('url'));
+});
