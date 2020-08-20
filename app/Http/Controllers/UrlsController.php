d553e0e85cae11e64a2ae4c9b4aafbdbe2e6a004
Elhadji <damediagnea@gmail.com>
2020-08-20 11:48:27 +0200

    Ajout du fichier readme

--- app/Http/Controllers/UrlsController.php
+++ app/Http/Controllers/UrlsController.php
@@ -15,49 +15,31 @@ class UrlsController extends Controller
     public function store(Request $request){
 
         Request()->validate([
-            'url' =>['required','url'],
-           
+            'url' =>['required','url'], 
         ]);
     
         //Vérifier que l'url passer en argument n'a pas été raccourcie et la retourner si tel est le cas 
     
-    $url = Url::where('url', request('url'))->first();
-    
-    if($url){
-    
-        return view('result')->with('shortener', $url->shortener);
-        
-    }
-    
-    
-    $row = Url::create([
-    
-        'url' => request('url'),
-        'shortener' =>Url::get_unique_short_url()
-    ]);
-    
-    if($row){
-    
-        return view('result')->with('shortener', $row->shortener);
-    }else{
-    
-        return   redirect('/');
-    
-    }
+      $record = $this->getRecordUrl($request->url);
+
+      return view('result')->withShortener($record->shortener);
+
     
     }
 
     public function show($shortener){
 
-        $url = Url::where('shortener',$shortener)->first();
-
-        if(! $url){
-    
-          return   redirect('/');
-            
-        }else{
-    
-            return redirect($url->url);
-        }
+        $url = Url::where('shortener',$shortener)->firstOrFail();
+        return redirect($url->url);
     }
+
+    private function getRecordUrl($url){
+
+        return Url::firstOrcreate(
+            ['url' =>$url],['shortener' =>Url::get_unique_short_url()]
+            
+        );
+    }
+
+   
 }
